f78d8b4964e71a6b990b7d709e6aebcdf06a0c15
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-26 19:04:21 -0600

    fix typo, add query to get all properties limited to num

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
-  database: 'bootcampx'
+  database: 'lightbnb'
 });
 
 pool.connect(() => {
@@ -82,11 +82,17 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+  return pool  
+    .query(
+      `SELECT * FROM properties LIMIT $1;`, 
+      [limit])
+    .then((result) => {
+      return result.rows
+    })
+    .catch(err => {
+      return err.message;
+    });
 }
 exports.getAllProperties = getAllProperties;
 
