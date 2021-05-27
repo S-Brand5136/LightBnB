5671fe37bbabb622a1c46c63a7f385ba9615975d
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-27 14:55:12 -0600

    Add query to filter listing searches

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -128,24 +128,53 @@ const getAllProperties = function (options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id`;
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
-  // if (options.city) {
-  //   queryParams.push(`%${options.city}%`);
-  //   queryString += `WHERE city LIKE $${queryParams.length} `;
-  // }
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryParams.push(options.maximum_price_per_night * 100);
+    if (queryParams.length === 3) {
+      queryString += `AND cost_per_night >= $${queryParams.length - 1} 
+      AND cost_per_night <= $${queryParams.length}`;
+    } else {
+      queryString += `WHERE cost_per_night >= $${queryParams.length - 1} 
+      AND cost_per_night <= $${queryParams.length}`;
+    }
+  }
+
+  if (options.minimum_price_per_night && queryParams.length <= 1) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    if (queryParams.length === 2) {
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+    } else {
+      queryString += `WHERE cost_per_night >= $${queryParams.length} `;
+    }
+  }
 
   if (options.owner_id) {
     queryParams.push(options.owner_id);
-    queryString += ` WHERE owner_id = $1 `;
+    queryString += `WHERE owner_id = $${queryParams.length} `;
+  }
+
+  queryString += `
+  GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
   }
 
   queryParams.push(limit);
   queryString += `
-  GROUP BY properties.id
   ORDER BY cost_per_night
-  LIMIT $${queryParams.length};
-  `;
+  LIMIT $${queryParams.length};`;
 
   console.log(queryString, queryParams);
 
