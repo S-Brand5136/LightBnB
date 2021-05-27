cde73407744cd2ae3e5f08f067b04bce0fa6caea
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-27 12:53:07 -0600

    add query to show the logged in users reservations by latest

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -96,7 +96,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool  
+    .query(
+      `SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+      FROM reservations
+      JOIN users ON users.id = guest_id
+      JOIN properties ON properties.id = property_id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1 AND end_date < now()::date
+      GROUP BY reservations.id, properties.id
+      ORDER BY reservations.start_date DESC
+      LIMIT $2;`, 
+      [`${guest_id}`, limit])
+    .then((result) => {
+      return result.rows
+    })
+    .catch(err => {
+      return err.message;
+    });
 }
 exports.getAllReservations = getAllReservations;
 
